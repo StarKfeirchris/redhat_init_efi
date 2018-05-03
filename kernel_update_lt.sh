c2e507e636034e0d3fb212ea5eb714915e029ce9
Star Kfeirchris <tx01049847@hotmail.com>
2018-05-04 01:13:55 +0800

    :lock: :bug: kernel_update_*.sh: 將自動重開機部分改成先詢問要不要重開。

--- kernel_update_lt.sh
+++ kernel_update_lt.sh
@@ -9,5 +9,19 @@ yum -y --enablerepo=elrepo-kernel install kernel-lt
 
 grub2-set-default 0
 
-init 6
+# Reboot system.
+read -p "Longterm kernel update succeed, do you want to reboot?(yes or no) " reboot
+
+case ${reboot} in
+        yes|y|Y|Yes|yEs|yeS|YEs|yES|YES|YeS )
+                init 6
+                ;;
+        no|n|N|NO|nO|No )
+                echo 'Will not reboot system.'
+                ;;
+        * )
+                echo 'Entered a character other than yes or no, script will be stop now.'
+                ;;
+
+esac
 
