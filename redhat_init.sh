be967c64e45dfe55e7af5a819f98d1545442066d
Star Kfeirchris <tx01049847@hotmail.com>
2018-05-29 22:57:41 +0800

    :sparkles: redhat_init.sh: 從現在開始，確保重複執行不會出現意想不到的結果，可一直重複執行。

--- redhat_init.sh
+++ redhat_init.sh
@@ -13,7 +13,16 @@ yum install -y redhat-lsb --skip-broken
 redhat_release=$(lsb_release -irs)
 
 # Backup bashrc file.
-\cp -f /etc/bashrc /etc/bashrc.original
+bashrc_backup=$(ls /etc | grep -E 'bashrc.bak|bashrc.original' || true)
+
+case ${bashrc_backup} in
+	'' )
+		\cp -f /etc/bashrc /etc/bashrc.original
+		;;
+	'bashrc.'* )
+		true
+		;;
+esac
 
 # Execute command for different system versions.
 # Disable iptables or firewall. (Default not disable)
@@ -22,60 +31,87 @@ redhat_release=$(lsb_release -irs)
 # Upgrade kernel to mainline.
 case ${redhat_release} in
 	'CentOS 6.'* )
-		# If you need close iptables, remove comment.
-		#chkconfig iptables off
-		sed -i '36 s/  /  #/g' /etc/bashrc
-		sed -i '36a [ "$PS1" = "\\\\\s-\\\\\/v\\\\\\$ " ] && PS1="\\[\\e[0;91m\\][\\[\\e[0m\\]\\[\\e[0;92m\\]\\u\\[\\e[0m\\]\\[\\e[0;94m\\]@\\h\\[\\e[0m\\] \\[\\e[0;93m\\]\\W/\\[\\e[0m\\]\\[\\e[0;91m\\]]\\[\\e[0m\\]\\[\\e[0;93m\\]\\\\\$\\[\\e[0m\\] "' /etc/bashrc
-		sed -i '37 s/^/   /' /etc/bashrc
-		sed -i '37 s/\/v/v/' /etc/bashrc
-		yum install -y epel-release
-		yum update -y
-		yum upgrade -y
-		rpm -Uvh http://www.elrepo.org/elrepo-release-6-8.el6.elrepo.noarch.rpm
-		rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-		yum -y --enablerepo=elrepo-kernel install kernel-ml
-		sed -i '11 s/default=1/default=0/' /boot/efi/EFI/redhat/grub.conf
+		# First run or not.
+		first_run=$(cat /etc/bashrc | grep '0;94m' | cut -f13 -d'[' | cut -f1 -d'\' || true)
+		case ${first_run} in
+			'' )
+				# If you need close iptables, remove comment.
+				#chkconfig iptables off
+				sed -i '36 s/  /  #/g' /etc/bashrc
+				sed -i '36a [ "$PS1" = "\\\\\s-\\\\\/v\\\\\\$ " ] && PS1="\\[\\e[0;91m\\][\\[\\e[0m\\]\\[\\e[0;92m\\]\\u\\[\\e[0m\\]\\[\\e[0;94m\\]@\\h\\[\\e[0m\\] \\[\\e[0;93m\\]\\W/\\[\\e[0m\\]\\[\\e[0;91m\\]]\\[\\e[0m\\]\\[\\e[0;93m\\]\\\\\$\\[\\e[0m\\] "' /etc/bashrc
+				sed -i '37 s/^/   /' /etc/bashrc
+				sed -i '37 s/\/v/v/' /etc/bashrc
+				yum install -y epel-release
+				yum update -y
+				yum upgrade -y
+				rpm -Uvh http://www.elrepo.org/elrepo-release-6-8.el6.elrepo.noarch.rpm || true
+				rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org || true
+				yum -y --enablerepo=elrepo-kernel install kernel-ml
+				sed -i '11 s/default=1/default=0/' /boot/efi/EFI/redhat/grub.conf
+				;;
+			'0;94m' )
+				true
+				;;
+		esac
 		;;
 	'CentOS 7.'* )
-		# If you need close firewall, remove comment.
-		#systemctl disable firewalld
-		sed -i '41 s/  /  #/g' /etc/bashrc
-		sed -i '41a [ "$PS1" = "\\\\\s-\\\\\/v\\\\\\$ " ] && PS1="\\[\\e[0;91m\\][\\[\\e[0m\\]\\[\\e[0;92m\\]\\u\\[\\e[0m\\]\\[\\e[0;94m\\]@\\h\\[\\e[0m\\] \\[\\e[0;93m\\]\\W/\\[\\e[0m\\]\\[\\e[0;91m\\]]\\[\\e[0m\\]\\[\\e[0;93m\\]\\\\\$\\[\\e[0m\\] "' /etc/bashrc
-		sed -i '42 s/^/   /' /etc/bashrc
-		sed -i '42 s/\/v/v/' /etc/bashrc
-		yum install -y epel-release
-		yum update -y
-		yum upgrade -y
-		rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
-		rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-		yum -y --enablerepo=elrepo-kernel install kernel-ml
-		grub2-set-default 0
+		# First run or not.
+		first_run=$(cat /etc/bashrc | grep '0;94m' | cut -f13 -d'[' | cut -f1 -d'\' || true)
+		case ${first_run} in
+			'' )
+				# If you need close firewall, remove comment.
+				#systemctl disable firewalld
+				sed -i '41 s/  /  #/g' /etc/bashrc
+				sed -i '41a [ "$PS1" = "\\\\\s-\\\\\/v\\\\\\$ " ] && PS1="\\[\\e[0;91m\\][\\[\\e[0m\\]\\[\\e[0;92m\\]\\u\\[\\e[0m\\]\\[\\e[0;94m\\]@\\h\\[\\e[0m\\] \\[\\e[0;93m\\]\\W/\\[\\e[0m\\]\\[\\e[0;91m\\]]\\[\\e[0m\\]\\[\\e[0;93m\\]\\\\\$\\[\\e[0m\\] "' /etc/bashrc
+				sed -i '42 s/^/   /' /etc/bashrc
+				sed -i '42 s/\/v/v/' /etc/bashrc
+				yum install -y epel-release
+				yum update -y
+				yum upgrade -y
+				rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm || true
+				rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org || true
+				yum -y --enablerepo=elrepo-kernel install kernel-ml
+				grub2-set-default 0
+				;;
+			'0;94m' )
+				true
+				;;
+		esac
 		;;
 	'Fedora '* )
-		sed -i '45 s/  /   #/g' /etc/bashrc
-		sed -i '45a [ "$PS1" = "\\\\\s-\\\\\/v\\\\\\$ " ] && PS1="\\[\\e[0;91m\\][\\[\\e[0m\\]\\[\\e[0;92m\\]\\u\\[\\e[0m\\]\\[\\e[0;94m\\]@\\h\\[\\e[0m\\] \\[\\e[0;93m\\]\\W/\\[\\e[0m\\]\\[\\e[0;91m\\]]\\[\\e[0m\\]\\[\\e[0;93m\\]\\\\\$\\[\\e[0m\\] "' /etc/bashrc
-		sed -i '46 s/^/    /' /etc/bashrc
-		sed -i '46 s/\/v/v/' /etc/bashrc
-		echo '
-		#!/bin/bash
-		# THIS FILE IS ADDED FOR COMPATIBILITY PURPOSES
-		#
-		# It is highly advisable to create own systemd services or udev rules
-		# to run scripts during boot instead of using this file.
-		#
-		# In contrast to previous versions due to parallel execution during boot
-		# this script will NOT be run after all other services.
-		#
-		# Please note that you must run 'chmod +x /etc/rc.d/rc.local' to ensure
-		# that this script will be executed during boot.
-		
-		' >> /etc/rc.d/rc.local
-		# If you need close firewall, remove comment.
-		#systemctl disable firewalld
-		#chmod +x /etc/rc.d/rc.local
-		#echo "systemctl stop firewalld" >> /etc/rc.d/rc.local
-		dnf update -y
-		dnf upgrade -y
+		# First run or not.
+		first_run=$(cat /etc/bashrc | grep '0;94m' | cut -f13 -d'[' | cut -f1 -d'\' || true)
+		case ${first_run} in
+			'' )
+				sed -i '45 s/  /   #/g' /etc/bashrc
+				sed -i '45a [ "$PS1" = "\\\\\s-\\\\\/v\\\\\\$ " ] && PS1="\\[\\e[0;91m\\][\\[\\e[0m\\]\\[\\e[0;92m\\]\\u\\[\\e[0m\\]\\[\\e[0;94m\\]@\\h\\[\\e[0m\\] \\[\\e[0;93m\\]\\W/\\[\\e[0m\\]\\[\\e[0;91m\\]]\\[\\e[0m\\]\\[\\e[0;93m\\]\\\\\$\\[\\e[0m\\] "' /etc/bashrc
+				sed -i '46 s/^/    /' /etc/bashrc
+				sed -i '46 s/\/v/v/' /etc/bashrc
+				echo '
+#!/bin/bash
+# THIS FILE IS ADDED FOR COMPATIBILITY PURPOSES
+#
+# It is highly advisable to create own systemd services or udev rules
+# to run scripts during boot instead of using this file.
+#
+# In contrast to previous versions due to parallel execution during boot
+# this script will NOT be run after all other services.
+#
+# Please note that you must run 'chmod +x /etc/rc.d/rc.local' to ensure
+# that this script will be executed during boot.
+
+				' >> /etc/rc.d/rc.local
+				# If you need close firewall, remove comment.
+				#systemctl disable firewalld
+				#chmod +x /etc/rc.d/rc.local
+				#echo "systemctl stop firewalld" >> /etc/rc.d/rc.local
+				dnf update -y
+				dnf upgrade -y
+				;;
+			'0;94m' )
+				true
+				;;
+		esac
 		;;
 esac
 
@@ -86,10 +122,24 @@ yum install -y vim bash-completion net-tools wget screen ntpdate
 ntpdate time.stdtime.gov.tw && hwclock -w
 
 # Auto update time.
-echo "* */12 * * * root /usr/sbin/ntpdate time.stdtime.gov.tw && /sbin/hwclock -w" >> /etc/crontab
+time_sync=$(cat /etc/crontab | grep 'stdtime' | cut -f5 -d'/' | cut -f2 -d'.' || true)
+
+case ${time_sync} in
+	'' )
+		echo "* */12 * * * root /usr/sbin/ntpdate time.stdtime.gov.tw && /sbin/hwclock -w" >> /etc/crontab
+		;;
+	'stdtime' )
+		true
+		;;
+esac
 
 # Add history time.
-echo '
+# Only the first run will be written, other not.
+his_time_conf=$(cat /etc/bashrc | grep 'History Time' | cut -f2-3 -d' ' || true)
+
+case ${his_time_conf} in
+	'' )
+		echo '
 
 ######################## History Time ########################
 # History time
@@ -104,21 +154,29 @@ export HISTTIMEFORMAT="%m/%d/%y %a %H:%M:%S -> "
 #History saved commad line
 export HISTSIZE=20000
 ##############################################################
-' >> /etc/bashrc
-
-# Effective bashrc.
-source /etc/bashrc
-
-# Backup sysctl.conf
-\cp -f /etc/sysctl.conf /etc/sysctl.conf.original
+		' >> /etc/bashrc
+		;;
+	'History Time' )
+		true
+		;;
+esac
 
 # Add TCP BBR config
-echo '
+sysctl_backup=$(cat /etc/sysctl.conf | grep BBR | cut -f2 -d' ' || true)
 
+case ${sysctl_backup} in
+	'' )
+		echo '
+		
 # BBR congestion control
 net.core.default_qdisc = fq
 net.ipv4.tcp_congestion_control = bbr
-' >> /etc/sysctl.conf
+		' >> /etc/sysctl.conf
+		;;
+	'BBR' )
+		true
+		;;
+esac
 
 # Reboot system.
 set +x
@@ -134,7 +192,5 @@ case ${reboot} in
 		;;
 	* )
 		echo 'Entered a character other than yes or no, script will be stop now.'
-		;;
-		
+		;;	
 esac
-
